3e174bd5c729490131f7090aa4be730d788615c0
Nirbheek Chauhan <nirbheek@centricular.com>
2025-01-30 19:40:46 +0530

    pango.recipe: Fix missing files warnings
    introspection support was disabled, but the files were listed.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1609>

--- cerbero/build/filesprovider.py
+++ cerbero/build/filesprovider.py
@@ -484,8 +484,8 @@ class FilesProvider(object):
             return f
         for py_prefix in self.py_prefixes:
             pydir = os.path.basename(os.path.normpath(py_prefix))
-            pyversioname = re.sub(r"python|\.", '', pydir)
-            cpythonname = "cpython-" + pyversioname
+            pyversioname = re.sub(r'python|\.', '', pydir)
+            cpythonname = 'cpython-' + pyversioname
 
             splitedext = os.path.splitext(f)
             for ex in ['', 'm']:
