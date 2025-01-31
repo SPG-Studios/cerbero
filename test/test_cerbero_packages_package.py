17b0aa2a0ca8f59f0ba458bf04189f4083c1c5b6
L. E. Segovia <amy@centricular.com>
2025-01-31 09:24:37 -0300

    filesprovider: Don't look la files up if not under Android

--- test/test_cerbero_packages_package.py
+++ test/test_cerbero_packages_package.py
@@ -89,22 +89,19 @@ class PackageTest(unittest.TestCase):
 
     def testDevelFilesList(self):
         add_files(self.tmp)
-        devfiles = ['lib/libgstreamer-0.10.a', 'lib/libgstreamer-0.10.la']
+        devfiles = ['lib/libgstreamer-0.10.a']
         linuxdevfiles = devfiles + [
             'lib/libgstreamer-0.10.so',
             'lib/libgstreamer-x11.a',
-            'lib/libgstreamer-x11.la',
             'lib/libgstreamer-x11.so',
         ]
         windevfiles = devfiles + [
             'lib/libgstreamer-win32.a',
             'lib/libgstreamer-win32.dll.a',
-            'lib/libgstreamer-win32.la',
             'lib/libgstreamer-win32.def',
             'lib/gstreamer-win32.lib',
             'lib/libtest.a',
             'lib/libtest.dll.a',
-            'lib/libtest.la',
             'lib/libtest.def',
             'lib/test.lib',
             'lib/libgstreamer-0.10.dll.a',
